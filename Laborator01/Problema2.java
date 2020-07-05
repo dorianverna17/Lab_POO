e4859b37bfa2c1a65d5dec5101c4ae161f672326
Student USO VM User <student@stud.acs.upb.ro>
2020-07-05 18:33:15 +0300

    Firs commit

---
+++ Laborator01/Problema2.java
@@ -0,0 +1,11 @@
+class Problema2 {
+	
+	public void print(String str) {
+		System.out.println(str);
+	}
+
+	public static void main(String args[]) {
+		Problema2 obiect = new Problema2();
+		obiect.print("Test");
+	}
+}
