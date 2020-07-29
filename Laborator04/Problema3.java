bd8104184e893f22a854bb26c45889780a7cfc7d
Dorian Verna <dorianverna7@gmail.com>
2020-07-30 01:56:14 +0300

    Modified p3 in lab4

--- Laborator04/Problema3.java
+++ Laborator04/Problema3.java
@@ -2,17 +2,28 @@ import java.lang.*;
 import java.util.*;
 
 class HSet extends Hashtable {
+
 	//Adauga un element in multime, daca nu exista deja
 	public boolean add(Object value) {
-
+		if (super.contains(value)) 
+			return false;
+		else {
+			super.put(value, value);
+			return true;
+		}
 	}
 	//returneaza un String cu elementele multimii (doar cheile, nu perechi)
 	public String toString() {
-
+		return super.toString();
 	}
 	//Sterge perechea corespunzatoare cheii, intorcand valoarea
 	public Object remove(Object key) {
-		
+		if (super.containsKey(key)) {
+			super.remove(key);
+			return key;
+		}
+		else
+			return key;	
 	}
 }
 
