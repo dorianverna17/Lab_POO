156d0aa9bfa87373622052e7d9d8dcb579db8445
Dorian Verna <dorianverna7@gmail.com>
2020-08-19 23:22:52 +0300

    modified pb2 lab8

--- Laborator08/lab8/Task2.java
+++ Laborator08/lab8/Task2.java
@@ -1,6 +1,100 @@
-package lab08;
+//package lab08;
 
 import java.util.Comparator;
+import java.util.*;
+import java.lang.*;
+
+@SuppressWarnings("serial")
+class Catalog extends TreeSet<Catalog.Student> {
+    Comparator<Catalog.Student> comparator;
+
+    public Catalog(Comparator<Catalog.Student> comparator) {
+        this.comparator = comparator;
+    }
+
+    public Catalog() {
+        this.comparator = null;
+    }
+
+    public void addStudent(String name, double media, int clazz) {
+        this.add(new Student(name, media, clazz));
+        // Collections.sort(this, this.comparator);
+    }
+
+    public Student getStudent(String name) {
+        Iterator<Catalog.Student> iterator = this.iterator();
+        while(iterator.hasNext())
+            if (iterator.next().name.equals(name))
+                return iterator.next();
+        return null;
+    }
+
+    public void removeStudent(String name) {
+        Iterator<Catalog.Student> iterator = this.iterator();
+        while(iterator.hasNext()) {
+            Student stud = iterator.next();    
+            System.out.println(stud.name);
+            if (stud.name.equals(name)) {
+                this.remove(stud);
+                System.out.println(this);
+            }
+        }
+    }
+
+    public Catalog byClass(int clazz) {
+        @SuppressWarnings("serial")
+        Catalog cat = new Catalog(new Comparator<Catalog.Student>() {
+            @Override
+            public int compare(Catalog.Student o1, Catalog.Student o2) {
+                if (o1.media > o2.media) {
+                    return -1;
+                } else {
+                    if (o1.media < o2.media) {
+                        return 1;
+                    } else {
+                        if (o1.name.compareTo(o2.name) > 0) {
+                            return 1;
+                        }
+                    }
+                return -1;
+                }
+            }
+        });
+        Iterator<Catalog.Student> iterator = this.iterator();
+        while(iterator.hasNext())
+            if (iterator.next().media == clazz)
+                cat.addStudent(iterator.next().name, iterator.next().media, clazz);
+        return cat;
+    }
+
+    public class Student implements Comparable<Object> {
+        String name;
+        double media;
+        int clazz;
+
+        public Student(String name, double media, int clazz) {
+            this.name = name;
+            this.media = media;
+            this.clazz = clazz;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Student obj = (Student) o;
+            if (this.media > obj.media)
+                return 1;
+            else if (this.media < obj.media)
+                return -1;
+            else if (this.name.compareTo(obj.name) > 0)
+                return 1;
+            return -1;
+        }
+
+        public String toString() {
+            return this.name + " " + this.media + " " + this.clazz;
+        }
+    }
+}
 
 public class Task2 {
 
@@ -16,7 +110,18 @@ public class Task2 {
             @Override
             public int compare(Catalog.Student o1, Catalog.Student o2) {
                 // TODO: Sort by average (descending) and by name (ascending).
-               
+                if (o1.media > o2.media) {
+                    return 1;
+                } else {
+                    if (o1.media < o2.media) {
+                        return -1;
+                    } else {
+                        if (o1.name.compareTo(o2.name) > 0) {
+                            return -1;
+                        }
+                    }
+                }
+                return 1;
             }
         });
         catalog2.addAll(catalog);
@@ -42,6 +147,7 @@ public class Task2 {
         }
 
         catalog.removeStudent("Ionut");
+        System.out.println("Continutul colectiei: " + catalog);
         if (catalog.size() != 4) {
             System.err.println("Catalog.remove() a fost implementata gresit.");
         }
