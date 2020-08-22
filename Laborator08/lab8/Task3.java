8b63b2e24ed5e04c5d192cdf9275122904ba0eb3
Dorian Verna <dorianverna7@gmail.com>
2020-08-22 14:11:00 +0300

    p3 commit

--- Laborator08/lab8/Task3.java
+++ Laborator08/lab8/Task3.java
@@ -4,44 +4,78 @@ import java.util.Iterator;
 import java.util.*;
 import java.lang.*;
 
-class LinkedList<K> implements Iterable {
-    Node<K> first;
-    Node<K> last;
+class LinkedList<T> implements Iterable<T> {
+    Node<T> first;
+    Node<T> last;
 
-    private static class Node<K> {
-        K value;
-        Node<K> next;
+    public LinkedList() {
+        this.first = null;
+        this.last = null;
+    }
 
-        public Node(K value, Node next) {
+    static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
             this.value = value;
             this.next = next;
         }
 
-        public Node(K value) {
+        public Node(T value) {
             this.value = value;
             this.next = null;
         }
     }
 
-    class ListIterator implements Iterator {
+    class ListIterator<T> implements Iterator<T> {
+        Node<T> node;
+
+        public ListIterator(LinkedList<T> list) {
+            this.node = list.first;
+        }
+
+        public boolean hasNext() {
+            if (this.node == null)
+                return false;
+            return true;
+        }
+
+        public T next() {
+            T value;
+            value = this.node.value;
+            this.node = node.next;
+            return value;
+        }
 
     }
 
     //Insereaza un nod la inceputul listei
     public void addFirst(T data) {
-
+        Node<T> new_node = new Node<T>(data, this.first);
+        this.first = new_node;
     }
     //Insereaza un nod la sfarsitul listei
     public void add(T data) {
-
+        Node<T> new_node = new Node<T>(data);
+        if (this.last == null && this.first == null) {
+            this.first = new_node;
+            this.last = new_node;
+            this.last.next = null;
+        } else {
+            this.last.next = new_node;
+            this.last = new_node;
+            this.last.next = null;
+        }
     }
     //Returneaza primul nod din lista
     public T getNode() {
-
+        return this.first.value;
     }
     //Returneaza un obiect de tip ListIterator
     public Iterator<T> iterator() {
-        
+        Iterator<T> it = new ListIterator<T>(this);
+        return it;
     }    
 }
 
@@ -49,13 +83,21 @@ public class Task3 {
 
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<>();
+        // System.out.println("da");
         list.add(10);
+        // System.out.println("da");
         list.add(20);
+        // System.out.println("da");
         list.addFirst(5);
+        // System.out.println("da");
         list.add(22);
+        // System.out.println("da");
         list.add(25);
+        // System.out.println("da");
         list.add(30);
+        // System.out.println("da");
 
+        // System.out.println(list.first.next.next.next.next.next.next.value);
         int last = 0;
         System.out.println("Afisare 1");
         for (Integer i : list) {
